245c7fcd450d95a0b2d98f953e66afae64e87d13
cristoferscalante <cristoferscalante@gmail.com>
2024-05-30 00:16:09 +0000

    clase 29052024

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-const dotenv = require('dotenv')
 dotenv.config();
+
 const app = require('./app');
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })	
-.then((result) => app.listen(3000))
-.catch((err) => console.log(err));
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
-
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
